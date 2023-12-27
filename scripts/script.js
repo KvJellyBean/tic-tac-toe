415ee80db35012df30d980ebd94727d922ae0562
KvJellyBean <kevinnatan91@gmail.com>
2023-12-27 20:57:36 +0700

    Refactor code and complete display functionality
    Refactor some code in board and game object, also complete the displayController functionality

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,10 @@
 const GameBoard = (() => {
     let board = new Array(9).fill("");
 
+    // Getter Setter
     const getBoard = () => board;
 
-    const placeXO = (player, index) => {
+    const setBoard = (player, index) => {
         board[index] = player;
     };
 
@@ -15,7 +16,7 @@ const GameBoard = (() => {
 
     return {
         getBoard,
-        placeXO,
+        setBoard,
         resetBoard
     }
 })();
@@ -30,48 +31,79 @@ const Player = (name, mark) => {
 
 // Game Object
 const Game = (() => {
-    const players = [Player('Kevin', 'X'), Player('Natanael', 'O')];
-    let currentPlayer = players[0];
+    // Variables
+    let players;
+    let currentPlayer;
+    let round = 0;
+    let gameOver = false;
+    let winner;
+
+    // Getter Setter
+    const getPlayer = () => players;
+
+    const setPlayer = (player1 = 'Player 1', player2 = 'Player 2') => {
+        players = [Player(player1, 'X'), Player(player2, 'O')];
+    };
 
     const getCurrentPlayer = () => currentPlayer;
 
+    const setCurrentPlayer = (player) => currentPlayer = player;
+
+    const getRound = () => round;
+
+    const setRound = (rounds) => round = rounds;
+
+    const getWinner = () => winner;
+
+    const setWinner = (player) => winner = player;
+
+    // Function to change player's turn
     const _switchPlayerTurn = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+
+    // Function to randomize who play first
+    const randomFirstTurn = () => {
+        let decider = Math.floor(Math.random() * 2);
+        decider == 0 ? setCurrentPlayer(players[0]) : setCurrentPlayer(players[1]);
     };
 
     // Function to play the game each step/round
     const playRound = (index) => {
         const board = GameBoard.getBoard();
-        if (index < 0 || index > 8) {
-            alert("Choice box's index between 0-8")
-            return;
-        }
 
+        // Check if there is already a mark inside
         if (board[index]) {
-            console.log('Try Again, the cell already filled in');
-            currentPlayer = getCurrentPlayer();
+            setCurrentPlayer(currentPlayer);
             return;
         } else {
-            console.log(`${getCurrentPlayer().name} place ${getCurrentPlayer().mark} in index ${index}`);
-            GameBoard.placeXO(getCurrentPlayer().mark, index);
-            _switchPlayerTurn();
+            GameBoard.setBoard(getCurrentPlayer().mark, index);
         }
 
-        // Check win condition -> if win, display winner and reset game.
-        if (_checkWin()) {
-            // condition want to play again(maybe show dialog)? : resetgame
-            resetGame();
+        // Winning/Tie checking
+        if (isWinning()) {
+            gameOver = true;
+            setWinner(getCurrentPlayer());
+            return;
         }
+        if (round === 9) {
+            gameOver = true;
+            return;
+        }
+
+        round++;
+        _switchPlayerTurn();
     };
 
     // Function to reset/restart the gmae
     const resetGame = () => {
-        currentPlayer = players[0];
+        gameOver = false;
+        randomFirstTurn();
         GameBoard.resetBoard();
     };
 
-    // Function to check winning/tie condition
-    const _checkWin = () => {
+    // Function to check winning condition
+    const isWinning = () => {
         const board = GameBoard.getBoard();
         const winState = [
             [0, 3, 6],
@@ -84,6 +116,7 @@ const Game = (() => {
             [2, 4, 6]
         ];
 
+        // Ensure the mark's indexes have a combination as winState's
         const checkWinner = (playerIndex) => {
             return winState.some(combination => combination.every(index => playerIndex.includes(index)));
         };
@@ -91,75 +124,147 @@ const Game = (() => {
         const xIndex = board.reduce((acc, cell, index) => (cell === 'X' ? [...acc, index] : acc), []);
         const oIndex = board.reduce((acc, cell, index) => (cell === 'O' ? [...acc, index] : acc), []);
 
-        if (xIndex.length >= 3 && checkWinner(xIndex)) {
-            alert("Congrats, X has won!");
-            return true;
-        } else if (oIndex.length >= 3 && checkWinner(oIndex)) {
-            alert("Congrats, O has won!");
-            return true;
-        } else if ((xIndex.length + oIndex.length) == 9) {
-            alert('TIE');
+        if ((xIndex.length >= 3 && checkWinner(xIndex)) || oIndex.length >= 3 && checkWinner(oIndex)) {
             return true;
         }
 
         return false;
     };
 
-    // const _checkWin = () => {
-    //     const winState = [
-    //         [0, 3, 6],
-    //         [1, 4, 7],
-    //         [2, 5, 8],
-    //         [0, 1, 2],
-    //         [3, 4, 5],
-    //         [6, 7, 8],
-    //         [0, 4, 8],
-    //         [2, 4, 6]
-    //     ]
-
-    //     let xIndex = [];
-    //     let oIndex = [];
-
-    //     // Check indexes of all X and O
-    //     for (let i = 0; i < board.length; i++) {
-    //         if (board[i] == 'X') {
-    //             xIndex.push(i);
-    //         } else if (board[i] == 'O') {
-    //             oIndex.push(i);
-    //         }
-    //     }
-
-    //     console.log(`x = [${xIndex}]`);
-    //     console.log(`o = [${oIndex}]`);
-    //     // winner
-    //     if (xIndex.length >= 2 || oIndex.length >= 2) {
-    //         for (let i = 0; i < winState.length; i++) {
-    //             if (xIndex.indexOf(winState[i][0]) >= 0) {
-    //                 if (xIndex.indexOf(winState[i][1]) >= 0) {
-    //                     if (xIndex.indexOf(winState[i][2]) >= 0) {
-    //                         console.log('yes');
-    //                         alert("Congrats, X have won!");
-    //                         return true;
-    //                     }
-    //                 }
-    //             }
-    //             if (oIndex.indexOf(winState[i][0]) >= 0) {
-    //                 if (oIndex.indexOf(winState[i][1]) >= 0) {
-    //                     if (oIndex.indexOf(winState[i][2]) >= 0) {
-    //                         alert("Congrats, O have won!");
-    //                         return true;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // };
+    const gameIsOver = () => gameOver;
 
     return {
+        setPlayer,
+        getPlayer,
+        setCurrentPlayer,
         getCurrentPlayer,
+        setRound,
+        getRound,
+        setWinner,
+        getWinner,
+        randomFirstTurn,
         playRound,
-        resetGame
+        resetGame,
+        isWinning,
+        gameIsOver
     }
 })();
 
+const displayController = (() => {
+    const _form = document.querySelector('form');
+    const _playground = document.querySelector('#playground');
+    const _board = document.querySelector('#board');
+    const _controller = document.querySelector('.controller');
+    const _restartBtn = document.querySelector('.controller button:nth-child(1)');
+    const _quitGameBtn = document.querySelector('.controller button:nth-child(2)');
+    const _result = document.querySelector('.result');
+    const _display = document.querySelector('.display');
+    const _playAgainBtn = document.querySelector('.menu button:nth-child(1)');
+    const _quitMenuBtn = document.querySelector('.menu button:nth-child(2)');
+
+    // Game initialize
+    const initGame = () => {
+        _form.style.display = 'flex';
+        _playground.style.display = 'none';
+        _result.style.display = 'none';
+        const _formInput = _form.querySelectorAll('input');
+        _formInput.forEach(input => input.value = "");
+        _form.addEventListener('submit', _gameStart);
+    };
+
+    // Game Starting
+    const _gameStart = (e) => {
+        e.preventDefault();
+        _form.style.display = 'none';
+        _playground.style.display = 'flex';
+
+        const _player1 = document.querySelector('#name1').value;
+        const _player2 = document.querySelector('#name2').value;
+        Game.setPlayer(_player1, _player2);
+        Game.randomFirstTurn();
+        _renderBoard();
+
+        _restartBtn.addEventListener('click', _restartGame);
+        _quitGameBtn.addEventListener('click', _initNewGame);
+
+        const clickEvent = Game.gameIsOver() ? 'removeEventListener' : 'addEventListener';
+        _board[clickEvent]('click', _playTurn);
+    }
+
+    // Game action each turn/round
+    const _playTurn = (e) => {
+        if (e.target.nodeName === 'BUTTON') {
+            Game.playRound(e.target.id);
+            _renderBoard();
+
+            if (Game.isWinning()) {
+                _showWinner();
+                _endGame();
+            }
+            if (Game.getRound() === 9) {
+                _showTie();
+                _endGame();
+            }
+        }
+    }
+
+    // Displaying Result
+    const _showDisplay = (text) => {
+        _display.innerText = text;
+    }
+
+    const _showWinner = () => {
+        _showDisplay(`${Game.getWinner().name}`);
+    }
+    const _showTie = () => {
+        _showDisplay('TIE');
+    }
+
+    // End game condition
+    const _endGame = () => {
+        _board.removeEventListener('click', _playTurn);
+        _controller.style.display = 'none';
+        _result.style.display = 'flex';
+        _playAgainBtn.addEventListener('click', _restartGame);
+        _quitMenuBtn.addEventListener('click', _initNewGame);
+    }
+
+    // Restart game condition
+    const _restartGame = () => {
+        _board.removeEventListener('click', _playTurn);
+        Game.resetGame();
+        Game.setRound(0);
+
+        _board.addEventListener('click', _playTurn);
+        _controller.style.display = 'flex';
+        _result.style.display = 'none';
+
+        _renderBoard();
+    }
+
+    // Reinitialize game
+    const _initNewGame = () => {
+        _restartGame();
+        initGame();
+    }
+
+    // Render the BoardGame
+    const _renderBoard = () => {
+        _board.innerHTML = '';
+
+        const board = GameBoard.getBoard();
+
+        board.forEach((cell, index) => {
+            const button = document.createElement('button');
+            button.innerText = cell;
+            button.setAttribute('id', `${index}`)
+            _board.appendChild(button);
+        });
+    };
+
+    return {
+        initGame,
+    }
+})();
+
+displayController.initGame();
